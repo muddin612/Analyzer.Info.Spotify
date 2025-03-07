1010c0a40a5f6ef5a9d68f1eabc58c9cb7515808
muddin <uddinm4@montclair.edu>
2025-03-06 23:51:06 -0500

    3/6

---
+++ src/main/java/com/example/Analyzer/Info/Spotify/Application.java
@@ -0,0 +1,13 @@
+package com.example.Analyzer.Info.Spotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
